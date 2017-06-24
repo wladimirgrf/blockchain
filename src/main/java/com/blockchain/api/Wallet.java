86245f45c1ebd0900f8d4b898b02d68ece43e9a0
Wladimir <wladimirgrf@gmail.com>
2017-06-24 00:45:53 -0300

    Master - change project architecture, alter blockchain info api consult to develop own blockchain explorer, methods read block, consult transaction and generate hash public wallet address

--- src/main/java/com/blockchain/api/Wallet.java
+++ src/main/java/com/blockchain/api/Wallet.java
@@ -3,82 +3,19 @@ package com.blockchain.api;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.blockchain.security.Password;
 import com.blockchain.security.Secret;
-import com.google.gson.JsonObject;
 
 
 public class Wallet {		
 
 	public static void main(String[] args){
-		//JsonObject teste = create("123", "cd0ebd41-3673-4096-8698-fdf738dbdf2e", null, "testeME", null);
-		//JsonObject teste = balance("c7d41c0a-1e60-4a62-8826-23992bc2d2c7", "123");
+		//JsonObject teste = create(null, "cd0ebd41-3673-4096-8698-fdf738dbdf2e", "KyotvhGiw1KwDY9ftaTmx6ugk833f6euExXFBMReq6Q2xFkJwbwx", null, null);
+		//JsonObject teste = balance("0b687393-9460-4eb6-a209-807c8ffb27fe", "x¯_(#%\u0004\b²7ZwTsl½lõ4ýi'\u000f\u000bë\u0007±À}ç3\u001b<½<Ä\u001fþtÒk", 4);
 
-		System.out.println("teste");
 	
 	}
 	
 	private Wallet() { }
  
-	public static JsonObject create (String password, String apiCode) {
-		return create(password, apiCode, null, null, null);
-	}
 	
-	public static JsonObject create (String password, String apiCode, String privateKey, String label, String email) {
-		JsonObject jsonObj = new JsonObject();
-		
-		if(apiCode == null || apiCode.isEmpty()){
-			jsonObj.addProperty("error", "api code is missing!");
-			return jsonObj;
-		}
-		
-		Map<String, String> params = new HashMap<String, String>();
-		
-		params.put("api_code", apiCode);
-
-		if(password == null || password.isEmpty()){
-			password = Password.create();
-		}
-		
-		params.put("password", password);
-		
-		if (privateKey != null) {
-			params.put("priv", privateKey);
-		}
-		if (label != null) {
-			params.put("label", label);
-		}
-		if (email != null) {
-			params.put("email", email);
-		}
-		
-		jsonObj = Service.post("api/v2/create", params);
-		
-		JsonObject encryption = Secret.encrypt(password);
-		
-		jsonObj.addProperty("password_enc", encryption.get("cipher_text").getAsString());
-		jsonObj.addProperty("password_key", encryption.get("key").getAsInt());
-		
-		return jsonObj;
-	}
-	
-	public static JsonObject balance (String guid, String password, int key) {
-		JsonObject jsonObj = new JsonObject();
-		
-		if(guid == null || guid.isEmpty()){
-			jsonObj.addProperty("error", "guid is missing!");
-			return jsonObj;
-		}
-		
-		if(password == null || password.isEmpty()){
-			jsonObj.addProperty("error", "password is missing!");
-			return jsonObj;
-		}
-		
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("password", Secret.decrypt(password, key));
-		
-		String uri = String.format("merchant/%s/balance", guid);
-		return Service.get(uri, params);
-    }
 }
--- src/main/java/com/blockchain/security/Secret.java
+++ src/main/java/com/blockchain/security/Secret.java
@@ -1,21 +1,23 @@
 package com.blockchain.security;
 
-import java.io.UnsupportedEncodingException;
+
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.log4j.Logger;
-
-import com.google.gson.JsonObject;
 
 
 public class Secret {
-	private final static Logger logger = Logger.getLogger(Secret.class);
 
     private static final String ALGORITHM = "AES";
     
@@ -24,45 +26,40 @@ public class Secret {
 	private Secret() { }
 	
     
-    public static JsonObject encrypt(String text) {
-    	BKey bKey = getBKey(0);
-    	byte[] plainText = text.getBytes(StandardCharsets.UTF_8);
-    	byte[] cipherByte = exec(Cipher.ENCRYPT_MODE, plainText, bKey);
-    	JsonObject jsonObj = null;
-    	try { 	
-    		String cipherText = new String(cipherByte, CHARSET);
-    		jsonObj = new JsonObject();
-    		jsonObj.addProperty("cipher_text", cipherText);
-        	jsonObj.addProperty("key", bKey.getKey());
-		} catch (UnsupportedEncodingException e) {
-			logger.error(e.getMessage());
-		}
-    	return jsonObj;
-    }
+//    public static JsonObject encrypt(String text) {
+//    	BKey bKey = getBKey(0);
+//    	JsonObject jsonObj = new JsonObject();
+//    	try { 	
+//    		byte[] plainText = text.getBytes(StandardCharsets.UTF_8);
+//    		byte[] cipherByte = exec(Cipher.ENCRYPT_MODE, plainText, bKey);
+//    		String cipherText = new String(cipherByte, CHARSET);
+//    		jsonObj.addProperty("cipher_text", cipherText);
+//        	jsonObj.addProperty("key", bKey.getKey());
+//		} catch (Exception e) {
+//			jsonObj.addProperty("error", e.toString());
+//		}
+//    	return jsonObj;
+//    }
 
-    public static String decrypt(String text, int key) {  
-    	BKey bKey = getBKey(key);
-    	byte[] cipherText;
-		try {
-			cipherText = text.getBytes(CHARSET);
-			return new String(exec(Cipher.DECRYPT_MODE, cipherText, bKey));
-		} catch (UnsupportedEncodingException e) {
-			logger.error(e.getMessage());
-		}
-		return null;
-    }
+//    public static JsonObject decrypt(String text, int key) {  
+//    	BKey bKey = getBKey(key);
+//    	JsonObject jsonObj = new JsonObject();
+//		try {
+//			byte[] cipherText = text.getBytes(CHARSET);
+//			String plainText = new String(exec(Cipher.DECRYPT_MODE, cipherText, bKey));
+//			jsonObj.addProperty("plain_text", plainText);
+//		} catch (Exception e) {
+//			jsonObj.addProperty("error", e.toString());
+//		}
+//		return jsonObj;
+//    }
     
-    private static byte[] exec(int mode, byte[] text, BKey bKey) {
-    	try{
-    		Cipher cipher 	 = Cipher.getInstance(ALGORITHM);
-    		byte[] encryptionKey 	= bKey.getValue().getBytes(StandardCharsets.UTF_8);
-	    	SecretKeySpec secretKey = new SecretKeySpec(encryptionKey, ALGORITHM);
-	    	cipher.init(mode, secretKey);
-	    	return cipher.doFinal(text);
-    	} catch (Exception e) {
-    		logger.error(e.getMessage());
-		}
-    	return null;
+    private static byte[] exec(int mode, byte[] text, BKey bKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException  {
+		Cipher cipher 	 = Cipher.getInstance(ALGORITHM);
+		byte[] encryptionKey 	= bKey.getValue().getBytes(StandardCharsets.UTF_8);
+    	SecretKeySpec secretKey = new SecretKeySpec(encryptionKey, ALGORITHM);
+    	cipher.init(mode, secretKey);
+    	return cipher.doFinal(text);
     }
 
 	private static BKey getBKey(int key){
