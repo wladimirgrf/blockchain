710a254703b76fdcc4980f1eb82f7b4db9f5b2b4
Wladimir <wladimirgrf@gmail.com>
2017-11-13 10:25:57 -0800

    Master - change structure to web application (REST API)

--- src/main/java/btc/blockchain/info/core/Wallet.java
+++ src/main/java/btc/blockchain/info/core/Wallet.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import com.google.gson.JsonObject;
 
 import btc.blockchain.security.Secret;
+import info.blockchain.api.blockexplorer.entity.FilterType;
 
 
 public class Wallet {
@@ -14,9 +15,9 @@ public class Wallet {
 		//JsonObject teste = create("123", "cd0ebd41367340968698fdf738dbdf2e", null, "testeME", null);
 		//JsonObject teste = balance("c7d41c0a1e604a62882623992bc2d2c7", "123");
 		//JsonObject teste = create(null, "cd0ebd41367340968698fdf738dbdf2e", "KyotvhGiw1KwDY9ftaTmx6ugk833f6euExXFBMReq6Q2xFkJwbwx", null, null);
-		JsonObject teste = balance("0b68739394604eb6a209807c8ffb27fe", "FUSXcYD41T7BzASv+KjqaHDmmLqt/W/iAPgDfzP63lCYooLwOAgKQ8IcISEQL439HdL/Tql+za0PZdrEa4ACpg==", 12);
+		//JsonObject teste = balance("0b68739394604eb6a209807c8ffb27fe", "FUSXcYD41T7BzASv+KjqaHDmmLqt/W/iAPgDfzP63lCYooLwOAgKQ8IcISEQL439HdL/Tql+za0PZdrEa4ACpg==", 12);
 
-		System.out.println(teste);
+		System.out.println("teste");
 
 	}
 
@@ -66,30 +67,51 @@ public class Wallet {
 		return jsonObj;
 	}
 
-	public static JsonObject balance (String guid, String password, int key) {
-		JsonObject jsonObj = new JsonObject();
+//	public static JsonObject balance (String guid, String password, int key) {
+//		JsonObject jsonObj = new JsonObject();
+//
+//		if(guid == null || guid.isEmpty()){
+//			jsonObj.addProperty("error", "guid is missing!");
+//			return jsonObj;
+//		}
+//
+//		if(password == null || password.isEmpty()){
+//			jsonObj.addProperty("error", "password is missing!");
+//			return jsonObj;
+//		}
+//
+//		Map<String, String> params = new HashMap<String, String>();
+//		JsonObject password_dec = Secret.decrypt(password, key);
+//		
+//		if(password_dec.get("plain_text") != null) {
+//			params.put("password", password_dec.get("plain_text").toString());
+//		} else {
+//			jsonObj.addProperty("error", password_dec.get("error").toString());
+//			return jsonObj;
+//		}
+//
+//		String uri = String.format("merchant/%s/balance", guid);
+//		return Service.get(uri, params);
+//	}
+	
+    public static JsonObject getAddress (String address, FilterType filter, Integer limit, Integer offset) {
+    		Map<String, String> params = new HashMap<String, String>();
+    		
+        if (filter != null) {
+            params.put("filter", filter.getFilterInt().toString());
+        }
+        if (limit != null) {
+            params.put("limit", limit.toString());
+        }
+        if (offset != null) {
+            params.put("offset", offset.toString());
+        }
 
-		if(guid == null || guid.isEmpty()){
-			jsonObj.addProperty("error", "guid is missing!");
-			return jsonObj;
-		}
+        String uri = String.format("rawaddr/%s", address);
+        return Service.get(uri, params);
+    }
 
-		if(password == null || password.isEmpty()){
-			jsonObj.addProperty("error", "password is missing!");
-			return jsonObj;
-		}
-
-		Map<String, String> params = new HashMap<String, String>();
-		JsonObject password_dec = Secret.decrypt(password, key);
-		
-		if(password_dec.get("plain_text") != null) {
-			params.put("password", password_dec.get("plain_text").toString());
-		} else {
-			jsonObj.addProperty("error", password_dec.get("error").toString());
-			return jsonObj;
-		}
-
-		String uri = String.format("merchant/%s/balance", guid);
-		return Service.get(uri, params);
-	}
+    public static JsonObject getAddress (String address) {
+        return getAddress(address, null, null, null);
+    }
 }
